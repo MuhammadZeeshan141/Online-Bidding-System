57151836639380e6dc3d2e17dc9b3d0ea26a2a3d
zeeshan <zeeshan4742@gmail.com>
2020-07-23 09:36:17 +0500

    Added Contact Module

--- contact/views.py
+++ contact/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+
+from account.views import validateEmail
 from .models import Contact
+from django.contrib import messages
 
 
 # Create your views here.
@@ -11,6 +14,13 @@ def contact(request):
         cont.email = request.POST['email']
         cont.subject = request.POST['subject']
         cont.message = request.POST['message']
-        cont.save()
+        if cont.name != '' and cont.email != '' and cont.subject != '' and cont.message != '':
+            if validateEmail(cont.email):
+                cont.save()
+                messages.success(request, 'Message sent successfully', extra_tags='alert-success')
+            else:
+                (messages.error(request, 'Enter a valid Email', extra_tags='alert-danger'))
+        else:
+            messages.error(request, 'Please fill all the fields', extra_tags='alert-danger')
 
     return render(request, "contact.html")
